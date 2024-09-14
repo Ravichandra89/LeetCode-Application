2f3cabd67982e71e45c641c89cf6518b2489606b
Ravi chandra <githubravi01@gmail.com>
2024-09-14 15:23:55 +0530

    Worker Node

--- src/app/api/worker.ts
+++ src/app/api/worker.ts
@@ -1,9 +1,36 @@
 import prisma from "@/prisma";
 import { createClient } from "redis";
-import Queue from "bull";
 
-// Redis Queue Clients
-const redisClient = createClient();
-const pubClient = createClient();
+// Redis & Pub Client
+const redisClient = createClient({ url: "redis://127.0.0.1:6379" });
+const pubClient = createClient({ url: "redis://127.0.0.1:6379" });
 
-// Create a bull queue 
+const startWorker = async () => {
+    try {
+        await redisClient.connect();
+        await pubClient.connect();
+
+        console.log("Worker Connected to Redis");
+
+        // Infinite Run function to take the tasks
+        try {
+            while(true){
+                const task = await redisClient.brPop("problems", 0);
+                if(task){
+                    // TODO: Send Task to MicroService Using REST API
+                }
+            }
+        } catch (error) {
+            console.error("Error While Poping Task", error);
+        }
+    } catch (error) {
+        console.error("Error in Redis Connection", error);
+    } finally{
+        await redisClient.disconnect();
+        await pubClient.disconnect();
+    };
+};
+
+// Execute startWroker function
+startWorker();
+
