b7e959cb6908aec025166220e24b46e4a17f41f5
Ravi chandra <githubravi01@gmail.com>
2024-09-28 23:49:51 +0530

    CURD : Delete route for Comment

--- src/app/api/problems/[id]/comments/[commentId]/route.ts
+++ src/app/api/problems/[id]/comments/[commentId]/route.ts
@@ -12,11 +12,11 @@ export async function PATCH(request: Request) {
 
     const { content } = await request.json();
 
-    if (!commentId) {
+    if (!commentId || !content) {
       return NextResponse.json(
         {
           success: false,
-          message: "Comment ID is required",
+          message: "Comment ID is required Or Content is Required",
         },
         { status: 404 }
       );
@@ -59,4 +59,47 @@ export async function PATCH(request: Request) {
   }
 }
 
-export async function DELETE(request: Request) {}
+export async function DELETE(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const commentId = searchParams.get("commentId");
+
+    if (!commentId) {
+      return NextResponse.json({
+        success: false,
+        message: "Content ID is required",
+      });
+    }
+
+    const deleteComment = await prisma.comment.delete({
+      where: { id: commentId },
+    });
+
+    if (!deleteComment) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Comment not found",
+        },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Comment deleted successfully",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error While Updating Problem", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error While Updating Problem",
+      },
+      { status: 500 }
+    );
+  }
+}
